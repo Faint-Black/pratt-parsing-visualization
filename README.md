2c25cfbfaed25ea65f600e57705b15c2c6ce5900
Cezar <fntblk@gmail.com>
2025-09-21 10:01:29 -0300

    initial dependency/import boilerplate done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pratt-parsing-visualization
-Visualizing the pratt parsing AST with a GUI.
+# Pratt Parsing Visualization
+Visualizing the pratt parsing abstract syntax tree with a GUI.
+
+Powered by Zig + Raylib.
+
+## Dependencies
+Just **Zig 0.15.1**
+
+## Building and Running
+```sh
+zig build run
+```
---
+++ src/main.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const rl = @import("raylib");
+
+pub fn main() !void {
+    const screenWidth = 800;
+    const screenHeight = 450;
+
+    rl.setTraceLogLevel(.err);
+    rl.initWindow(screenWidth, screenHeight, "Pratt Parsing");
+    defer rl.closeWindow();
+    rl.setTargetFPS(60);
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(.white);
+        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
+    }
+}
+
+test "test test" {
+    try std.testing.expectEqual(2, 1);
+}
