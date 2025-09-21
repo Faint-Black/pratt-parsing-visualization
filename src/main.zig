bc77ee8687f82f8681555645e8d2b1095b7d258d
Cezar <fntblk@gmail.com>
2025-09-21 11:13:56 -0300

    raygui

--- src/main.zig
+++ src/main.zig
@@ -1,23 +1,34 @@
 const std = @import("std");
 const rl = @import("raylib");
+const rg = @import("raygui");
 
 pub fn main() !void {
-    const screenWidth = 800;
-    const screenHeight = 450;
+    const screen_width = 800;
+    const screen_height = 450;
+    const frames_per_second = 60;
+    var frame_counter: u64 = 0;
+
+    const rect = rl.Rectangle{
+        .x = 10,
+        .y = 10,
+        .width = 300,
+        .height = 50,
+    };
+    var buffer: [64]u8 = undefined;
+    std.mem.copyForwards(u8, &buffer, "hello\x00");
 
     rl.setTraceLogLevel(.err);
-    rl.initWindow(screenWidth, screenHeight, "Pratt Parsing");
+    rl.initWindow(screen_width, screen_height, "Pratt Parsing");
     defer rl.closeWindow();
-    rl.setTargetFPS(60);
-    while (!rl.windowShouldClose()) {
+    rl.setTargetFPS(frames_per_second);
+    while (!rl.windowShouldClose()) : (frame_counter += 1) {
         rl.beginDrawing();
         defer rl.endDrawing();
-
         rl.clearBackground(.white);
-        rl.drawText("Congrats! You created your first window!", 190, 200, 20, .light_gray);
-    }
-}
 
-test "test test" {
-    try std.testing.expectEqual(1, 1);
+        _ = rg.textBox(rect, @as([:0]u8, @ptrCast(&buffer)), buffer.len - 1, true);
+        if (frame_counter % 60 == 0) {
+            std.debug.print("{s}\n", .{@as([*:0]u8, @ptrCast(&buffer))});
+        }
+    }
 }
