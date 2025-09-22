4fe32c34a3b5926a2c5b0a1a41a3a7c61b1c29eb
Cezar <fntblk@gmail.com>
2025-09-22 07:41:06 -0300

    window resizing

--- src/main.zig
+++ src/main.zig
@@ -4,21 +4,23 @@ const rg = @import("raygui");
 const render = @import("render.zig");
 
 pub fn main() !void {
-    var buffer: [256]u8 = undefined;
-    std.mem.copyForwards(u8, &buffer, "Type here\x00");
-    var read_text: []const u8 = undefined;
-
     rl.setTraceLogLevel(.err);
     rl.initWindow(render.screen_width, render.screen_height, "Pratt Parsing");
     defer rl.closeWindow();
+    rl.setWindowState(.{ .window_resizable = true });
     rl.setTargetFPS(render.frames_per_second);
-    while (!rl.windowShouldClose()) : (render.frame_counter += 1) {
+    while (!rl.windowShouldClose()) : ({
+        render.frame_counter += 1;
+        render.screen_width = rl.getScreenWidth();
+        render.screen_height = rl.getScreenHeight();
+    }) {
         rl.beginDrawing();
         defer rl.endDrawing();
         rl.clearBackground(.white);
-        read_text = render.updateTextBox(@ptrCast(&buffer), buffer.len - 1);
+
+        const textbox_contents = render.updateTextBox();
         if (render.frame_counter % render.frames_per_second == 0) {
-            std.debug.print("{s}\n", .{read_text});
+            std.debug.print("{s}\n", .{textbox_contents});
         }
     }
 }
--- src/render.zig
+++ src/render.zig
@@ -2,24 +2,25 @@ const std = @import("std");
 const rl = @import("raylib");
 const rg = @import("raygui");
 
-pub const screen_width = 800;
-pub const screen_height = 450;
+pub var screen_width: i32 = 800;
+pub var screen_height: i32 = 450;
 pub const frames_per_second = 60;
 pub var frame_counter: u64 = 0;
 
+var textbox_internal_buffer: [512]u8 = std.mem.zeroes([512]u8);
+
 /// renders and returns a readable slice of the textbox contents
-pub fn updateTextBox(buffer_ptr: [:0]u8, char_limit: comptime_int) []const u8 {
-    const textbox_pos = rl.Vector2{
-        .x = 10,
-        .y = 10,
-    };
+pub fn updateTextBox() []const u8 {
+    const raw_pointer: [:0]u8 = @ptrCast(&textbox_internal_buffer);
+    const textbox_pos_x = 10;
+    const textbox_pos_y = 10;
     const textbox_rect = rl.Rectangle{
-        .x = textbox_pos.x,
-        .y = textbox_pos.y,
-        .width = screen_width - textbox_pos.x * 2,
+        .x = textbox_pos_x,
+        .y = textbox_pos_y,
+        .width = @floatFromInt(screen_width - textbox_pos_x * 2),
         .height = 50,
     };
-    _ = rg.textBox(textbox_rect, buffer_ptr, char_limit, true);
-    const sentinel_pos = std.mem.indexOfSentinel(u8, 0, buffer_ptr);
-    return buffer_ptr[0..sentinel_pos];
+    _ = rg.textBox(textbox_rect, raw_pointer, textbox_internal_buffer.len - 1, true);
+    const sentinel_pos = std.mem.indexOfSentinel(u8, 0, raw_pointer);
+    return textbox_internal_buffer[0..sentinel_pos];
 }
